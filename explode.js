333cefd91833e910baa3ca991527f4b0d43ad1e8
Nommyde <rus@tam.pw>
2016-07-29 23:29:34 +0300

    semicolon

--- explode.js
+++ explode.js
@@ -1,11 +1,11 @@
 function * explode(word, p) {
-  p = p && p + " " || ""
+  p = p && p + " " || "";
 
-  yield p + word
+  yield p + word;
 
   for (let i = word.length; --i > 0;)
-    yield * explode(word.substr(i), p + word.substr(0, i))
+    yield * explode(word.substr(i), p + word.substr(0, i));
 }
 
 for (let exploded of explode("test"))
-  console.log(exploded)
+  console.log(exploded);
