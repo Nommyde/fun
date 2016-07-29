eb10b95025a5221abcae71868995402cb8882701
Nommyde <rus@tam.pw>
2016-07-29 22:08:01 +0300

    Create explode.js

---
+++ explode.js
@@ -0,0 +1,11 @@
+function * explode(word, p) {
+  p = p && p + " " || ""
+
+  yield p + word
+
+  for (let i = word.length; --i > 0;)
+    yield * explode(word.substr(i), p + word.substr(0, i))
+}
+
+for (let exploded of explode("test"))
+  console.log(exploded)
